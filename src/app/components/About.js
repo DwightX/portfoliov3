c3203ddc54b691328979b0f70fbeeec9ec97db8c
“Dwight <dwighthowichill@icloud.com>
2024-10-17 18:24:21 -0400

    updated about

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -3,7 +3,7 @@ const About = (props) => {
     <div className="container mx-auto max-w-screen-xl">
       <div className="section-Main">
         <div className=" flex flex-col px-16 pt-28">
-            <p>My interest in web development started back in 2006 when I tried to edit a custom MySpace Page - turns out hacking together a custom layout taught me a lot about HTML & CSS!</p>
+            <p>Software Engineer  with 5+ years of experience specializing in web application development and GIS solutions. Proven track record in developing responsive,modern JavaScript frameworks and mapping technologies. Expertise in frontend development, geospatial applications, and e-commerce solutions.</p>
 <p>Here are a few technologies I’ve been working with recently:</p>
         </div>
       </div>
